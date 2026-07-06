b040c9041d88b83b7f5edb836da76d3915cd2641
Kena Patel <kenapatel005@gmail.com>
2026-07-06 00:45:58 -0400

    final: PawPal+ fully tested and completed

--- test_pawpal.py
+++ test_pawpal.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pawpal_system import Task, Pet
+from pawpal_system import Task, Pet, Scheduler
 
 
 def test_mark_complete():
@@ -32,7 +32,6 @@ def test_add_task():
     )
 
     pet.add_task(task)
-
     assert len(pet.tasks) == 1
 
 
@@ -49,3 +48,17 @@ def test_recurring_task_daily():
     task.mark_complete()
 
     assert task.due_time == datetime(2026, 7, 6, 8, 0)
+
+
+def test_sort_tasks():
+    scheduler = Scheduler()
+
+    t1 = Task(1, "Late", "Walk", datetime(2026, 7, 6, 10, 0), 2)
+    t2 = Task(2, "Early", "Feed", datetime(2026, 7, 6, 8, 0), 1)
+
+    scheduler.add_task(t1)
+    scheduler.add_task(t2)
+
+    scheduler.sort_tasks()
+
+    assert scheduler.tasks[0].title == "Early"
